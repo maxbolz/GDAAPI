f9798c7b16375c6dd138a18f33a972883e2fcd1a
maxbolz <maxfbolz@gmail.com>
2026-07-06 18:47:47 -0400

    requirements, neon host

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
 import os
+from dotenv import load_dotenv
 from sqlmodel import create_engine, Session
 
+load_dotenv()
 
-# Adjust user/password/host/port/dbname to match your Postgres setup
 DB_USER = os.environ["DB_USER"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_NAME = os.environ["DB_NAME"]
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
 
 engine = create_engine(DATABASE_URL, echo=False)
 
